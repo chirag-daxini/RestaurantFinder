fa4046f9fbba9a94c165f68a3506a07fdeb7b0df
Chirag D <daxinichirag26@gmail.com>
2021-10-24 14:26:26 +0100

    added unit tests.

---
+++ RestaurantFinder.Tests/RestaurantServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using RestaurantFinder.Domain;
+using RestaurantFinder.Models;
+using System.Threading.Tasks;
+
+namespace RestaurantFinder.Tests
+{
+    [TestClass]
+    public class RestaurantServiceTests
+    {
+        private IRestaurantService _restautantService;
+        private AppSettings _appSettings;
+        private IHttpClientService _httpClientService;
+        [TestInitialize]
+        public void Intialize()
+        {
+            _appSettings = new AppSettings() { RestaurantServiceUrl = "https://uk.api.just-eat.io/" };
+            _httpClientService = Substitute.For<IHttpClientService>();
+            _restautantService = new RestaurantService(_appSettings, _httpClientService);
+        }
+
+        [TestMethod]
+        public async Task Should_Call_HttpClient_For_ValidData()
+        {
+            //arrange
+            string testCode = "EC4M 7RF";
+            string testURl = $"{_appSettings.RestaurantServiceUrl}{string.Format(AppConstants.ApplicationUrls.BYPOSTCODE, testCode)}";
+
+            //act
+            var result = await _restautantService.FindRestaurantbyCodeAsync(testCode);
+
+            //assert
+            await _httpClientService.Received().GetAsync<RestaurantResponse>(Arg.Is(testURl));
+        }
+    }
+}
---
+++ RestaurantFinder.Tests/ValidationServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestaurantFinder.Domain;
+using System.Threading.Tasks;
+
+namespace RestaurantFinder.Tests
+{
+    [TestClass]
+    public class ValidationServiceTests
+    {
+        private ValidationService _validationService;
+        [TestInitialize]
+        public void Intialize()
+        {
+            _validationService = new ValidationService();
+        }
+
+        [TestMethod]
+        public async Task Should_Return_True_For_ValidData_ValidatePostCode()
+        {
+            //arrange
+            string testCode = "EC4M 7RF";
+
+            //act
+            bool validationResult = await _validationService.ValidatePostCode(testCode);
+
+            //assert
+            Assert.IsTrue(validationResult);
+        }
+        [TestMethod]
+        public async Task Should_Return_False_For_InvalidData_ValidatePostCode()
+        {
+            string testCode = "EC4M";
+
+            //act
+            bool validationResult = await _validationService.ValidatePostCode(testCode);
+
+            //assert
+            Assert.IsFalse(validationResult);
+        }
+    }
+}
